8e797e88340c98cc5504e11ebecda75231d88f9a
adr29truck <adrianalmetunsmeds@gmail.com>
2019-01-15 21:30:26 +0100

    Code Cleanup

--- hangtheman_game.rb
+++ hangtheman_game.rb
@@ -8,7 +8,7 @@ end
 
 def play(word_letterincluded, blacklist)
     if word_letterincluded[-1].number?
-        word_positions = word_letterincluded.gsub(/\s+/m, ' ').strip.split(" ")
+        word_positions = word_letterincluded.gsub(/\s+/m, ' ').strip.split(' ')
         i = 0
         while i< word_positions.length
             word_positions[i] = word_positions[i].to_i
@@ -17,35 +17,35 @@ def play(word_letterincluded, blacklist)
         i = 0
         while i < word_positions.length
             raw = remove(solver(blacklist), word_positions[i]-1)
-            current_words_string = ""
+            current_words_string = ''
             iterations = 0
             while iterations < raw.length
                 current_words_string += raw[iterations]                      
                 iterations += 1
             end
             # Writes raw to file
-            File.write("words_temp.txt", current_words_string)   
+            File.write('words_temp.txt', current_words_string)   
             i += 1
         end
         sleep 1
         
-    elsif word_letterincluded.downcase == "n" || word_letterincluded.downcase == "no"
+    elsif word_letterincluded.downcase == 'n' || word_letterincluded.downcase == 'no'
         # Remove all words containing the selected letter
-        current_words_string = ""
+        current_words_string = ''
         iterations = 0
-        current_words = remove(solver("random input that dont matter").downcase, nil)
+        current_words = remove(solver('random input that dont matter').downcase, nil)
         while iterations < current_words.length
             current_words_string += current_words[iterations]
             iterations += 1
         end
-        File.write("words_temp.txt", current_words_string)     
+        File.write('words_temp.txt', current_words_string)     
     end
 end
 
 def play_length(word_length)
     if word_length.is_a? Integer
         # removes all words that have a length that is different from the one inputed
-        raw = File.readlines("words_temp.txt")
+        raw = File.readlines('words_temp.txt')
         iteration = 0
         while iteration < raw.length
             while raw[iteration] != nil && raw[iteration].length != word_length+1
@@ -54,21 +54,21 @@ def play_length(word_length)
             iteration += 1
         end
         # Writes raw to text file
-        current_words_string = ""
+        current_words_string = ''
         iterations = 0
         while iterations < raw.length
             current_words_string += raw[iterations]                      
             iterations += 1
         end
-        File.write("words_temp.txt", current_words_string)   
+        File.write('words_temp.txt', current_words_string)   
     else 
-        puts "An error occured"
+        puts 'An error occured'
     end
 end
 
 def remove(letter, position)
     iteration = 0
-    raw = File.readlines("words_temp.txt")
+    raw = File.readlines('words_temp.txt')
     if position == nil
         while raw[iteration] != nil
             if raw[iteration].chomp.include?(letter)
@@ -115,49 +115,49 @@ def hang_the_man(input)
     iterations = 0   
     blacklist = [] 
     
-    puts "\e[H\e[2J"
-    current_words = File.readlines("words.txt")
-    current_words_string = ""
+    puts '\e[H\e[2J'
+    current_words = File.readlines('words.txt')
+    current_words_string = ''
     while iterations < current_words.length
         current_words_string += current_words[iterations].chomp 
         if iterations < current_words.length
-            current_words_string += "\n"
+            current_words_string += '\n'
         end
         iterations += 1
     end
-    File.write("words_temp.txt", current_words_string.downcase)
-    puts "\e[H\e[2J"
-    print "How long is the word you would like me to guess?\n \n \n \n \n"
-    event.send_message "How long is the word you would like me to guess?"
+    File.write('words_temp.txt', current_words_string.downcase)
+    puts '\e[H\e[2J'
+    print 'How long is the word you would like me to guess?\n \n \n \n \n'
+    event.send_message 'How long is the word you would like me to guess?'
     word_length = gets.chomp
     play_length(word_length.to_i)
-    puts " Processing.."
+    puts ' Processing..'
     sleep 1
-    puts "\e[H\e[2J"
-    puts " Currently I only support the 100 most common words in the English language*"
-    puts " I´m calculating the best letter to start with..\n \n \n \n \n"
-    puts "* Based off EF.com"
+    puts '\e[H\e[2J'
+    puts ' Currently I only support the 100 most common words in the English language*'
+    puts ' I´m calculating the best letter to start with..\n \n \n \n \n'
+    puts '* Based off EF.com'
     sleep 1
     
-    words_left = File.readlines("words_temp.txt").length
+    words_left = File.readlines('words_temp.txt').length
     
     while words_left != 1
-        puts "\e[H\e[2J"
+        puts '\e[H\e[2J'
         sleep 2
-        puts "\e[H\e[2J"
-        puts "Do the word include the letter " + solver(blacklist).upcase.yellow + "?"
+        puts '\e[H\e[2J'
+        puts 'Do the word include the letter ' + solver(blacklist).upcase.yellow + '?'
         blacklist << solver(blacklist)
-        puts "(N)o or at what position in the word? If multiple positions separate them with a space\n \n \n \n"
+        puts '(N)o or at what position in the word? If multiple positions separate them with a space\n \n \n \n'
         word_letterincluded = gets.chomp
-        puts "I´m calculating the next letter.. ".green
+        puts 'I´m calculating the next letter.. '.green
         play(word_letterincluded, blacklist[0...-1])
-        words_left = File.readlines("words_temp.txt").length
+        words_left = File.readlines('words_temp.txt').length
         sleep 2
     end
-    print "I guess the word you choose was " + File.readlines("words_temp.txt")[0].to_s.chomp.upcase.green
+    print 'I guess the word you choose was ' + File.readlines('words_temp.txt')[0].to_s.chomp.upcase.green
 end
 
 
-# hang_the_man("random input")
+# hang_the_man('random input')
 
 
